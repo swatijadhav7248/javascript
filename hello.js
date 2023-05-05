2fec9d24732df3e59fa25faa5fd2066aaa5238d7
swatijadhav7248 <swatijadhav7248@gmail.com>
2023-05-05 16:55:55 +0530

    implemented hello.js

---
+++ hello.js
@@ -0,0 +1,28 @@
+
+
+
+
+console.log("Swati Jadhav");
+
+console.log("alandi.pune");
+
+console.log("Vaishnavi Rajpurohit");
+
+var firstName="Swati";
+console.log(firstName)
+
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
+
+
+
+
---
+++ variable_assign.js
@@ -0,0 +1,25 @@
+var myFullName; //variable Declaration
+console.log(myFullName);
+
+var myFullName;
+myFullName ="Swati Satish Jadhav";
+console.log(myFullName);
+
+var myLove;
+myLove ="I Love only JS";
+console.log(myLove);
+
+console.log("swati");
+
+console.log("Jadhav");
+
+console.log("22");
+
+console.log("Alandi,Pune");
+
+console.log("412105");
+
+console.log("Female");
+
+console.log("No");
+
