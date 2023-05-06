b9bc79ff42b12889bfec69996057f85e4dcbc609
swatijadhav7248 <swatijadhav7248@gmail.com>
2023-05-06 22:31:53 +0530

    1 and 2 assignment of javascript
    1 and 2 assignment of javascript.

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,45 @@
+
+
+
+
+// Primitive Type
+// 1. Number
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+console.log(age, weight, height, speckNumber);
+
+
+console.log(age);
+console.log(weight);
+console.log(height);
+console.log(speckNumber);
+
+// 2. Boolean --> true or false
+var isMarried = true;
+var knowAngular = true; 
+var isSoftwareEngineer = true;
+var isBachelor = false; 
+console.log(isBachelor);
+
+console.log(typeof isMarried);
+
+
+// 3. String --> Sequence of character enclosed in '' or ""
+var firstName = "Bill";
+var position = 'Microsoft Co-founder'; 
+var typeOfFirstName = typeof firstName;
+console.log( "Type of variable -> firstName: ", typeOfFirstName );
+console.log("Type of variable --> position: " , typeof position);
+
+//2. Special Data type
+// 1. Undefined
+var address;
+console.log("Address: ", address);
+console.log("type of variable address is", typeof address);
+
+// 2. null
+var pin = null;
+console.log("Value of variable -> pin: ", pin);
+console.log('Type of variable pin is:', typeof pin);
---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,62 @@
+console.log("/***Data Type Number***/")
+
+var height=5.4;
+console.log(height);
+console.log("Type of variable-->height",typeof height);
+
+var totalStudents=30;
+console.log(totalStudents);
+console.log("Type of variable-->totalStudents",typeof totalStudents);
+
+var marks=80% 
+console.log(marks);
+console.log("Type of variable-->marks",typeof marks);
+
+console.log("// =================================================================================================================================//")
+
+
+console.log("/***Data Type String***/")
+
+ var Name="Swati Jadhav";
+console.log(Name);
+console.log("Type of variable-->Name",typeof Name);
+
+var Education="BCS";
+console.log(Education);
+console.log("Type of variable-->Education",typeof Education);
+
+
+var mobileNumber="9822456723";
+console.log(mobileNumber);
+var typeOfMobileNumber= typeof mobileNumber;
+console.log("Type of variable-->mobileNumber",typeof mobileNumber);
+
+
+console.log("// =================================================================================================================================//")
+
+
+
+console.log("// ***Boolean****//")
+var isKnownAngular=true;
+console.log(isKnownAngular);
+console.log("Type of variable-->IsKnownAngular",typeof isKnownAngular);
+
+var isMarried=false;
+console.log(isMarried);
+console.log("Type of variable-->IsMarried",typeof isMarried);
+
+var isKnownJava=true;
+console.log(isKnownJava);
+console.log("Type of variable-->IsKnownJava",typeof isKnownJava);
+
+
+console.log("// =================================================================================================================================//")
+
+
+
+console.log("/***Difference Between undefined and null***/")
+console.log("undefined means variable is declared but it's value is not assigned and null means variable is declared but it's value is assigned as null value so we get typeof undefined as undefined and typeof null as object");
+
+
+
+
