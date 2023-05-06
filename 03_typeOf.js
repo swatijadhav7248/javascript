38c76b638b826604f6c524ee3037aa34346710c1
swatijadhav7248 <swatijadhav7248@gmail.com>
2023-05-06 23:26:55 +0530

    assignment 3 of javascript
    assignment 3 of javascript

---
+++ 03_typeOf.js
@@ -0,0 +1,63 @@
+console.log("/***Before Initialize Value***/")
+
+var bankName;
+console.log(bankName);
+
+
+console.log("/***After Initialize Value***/")
+
+
+bankName='SBI Bank';
+console.log("BankName: SBI Bank");
+console.log("Type of variable--> BankName",typeof bankName);
+
+
+console.log("// =================================================================================================================================//")
+
+
+
+
+var marks='90%';
+console.log("marks=90%");
+console.log("Type of variable--> Marks",typeof marks);
+
+
+console.log("// =================================================================================================================================//")
+
+
+
+
+console.log("/***Before Initialization***/");
+
+
+var isWorking;
+console.log(isWorking);
+
+
+
+console.log("/***After Initialization***/");
+
+
+
+isWorking=true;
+console.log("isWorking=true");
+
+
+
+console.log("// =================================================================================================================================//")
+
+
+
+
+
+
+
+
+var totalCount="Hundred and Seven";
+console.log("totalCount=Hundred and Seven");
+console.log("Type of variable-->TotalCount", typeof totalCount);
+
+
+
+
+
