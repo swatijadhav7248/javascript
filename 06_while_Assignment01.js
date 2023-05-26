76f21be864fcb08814745e54e4c4b32e3710f7ef
swatijadhav7248 <swatijadhav7248@gmail.com>
2023-05-26 16:30:55 +0530

    assignment of while loop
    assignment of while loop

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,110 @@
+// WAP to print numbers from 5 to 15 by incrementing 1
+
+
+console.log("1) WAP to print numbers from 5 to 15");
+let i=5; // initialization i = 1   2  3
+while (i<=15) { // condition 0<=15
+   console.log(i); // 0  1 2
+   i = i + 1;
+}
+
+
+
+console.log("----------------------------------------------------------------------------");
+
+
+
+console.log("2) WAP to print numbers from 50 to 40");
+
+
+
+let k = 50; 
+while (k>=40) {
+   console.log(k);
+   k--;
+}
+
+
+console.log("------------------------------------------------------------------------");
+
+
+
+
+console.log("3) WAP to print first 15 odd numbers");
+
+
+
+
+
+
+let j=1;  // initialization
+while (j<=15) {  // condition
+   if (j%2==1) {
+      console.log(j);
+   }
+   j++; // update expression
+}
+
+
+
+
+console.log("-----------------------------------------------------------------------------------");
+
+
+
+console.log("4) WAP to print first 10 even numbers");
+
+
+
+let a=0;  // initialization
+while (a<=10) {  // condition
+   if (a%2==0) {
+      console.log(a);
+   }
+   a++; // update expression
+}
+
+
+console.log("-----------------------------------------------------------------------");
+
+
+
+console.log("5) WAP to print 5 table of 5 like 5 10 15 20 25....50");
+
+let t=5;
+while(t<=50) {
+    console.log(t);
+    t=t+5;
+}
+
+
+console.log("----------------------------------------------------------------------------");
+
+
+
+console.log("6) WAP to print table of 10");
+
+
+let m=10;
+while(m<=100) {
+    console.log(m);
+    m=m+10;
+}
+
+
+console.log("-------------------------------------------------------------------------------");
+
+
+console.log("7) WAP to print table of 10 in reverse order");
+
+
+let l = 100; 
+while (l>=10) {
+   console.log(l);
+   l=l-10;
+   
+}
+
+
+
+
