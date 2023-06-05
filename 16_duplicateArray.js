92a0ac7e6bab765eb6cf35d926e8c1316b1e7ff1
swatijadhav7248 <swatijadhav7248@gmail.com>
2023-06-05 17:43:57 +0530

    implementing duplicate array file
    implementing duplicate array file

---
+++ 16_duplicateArray.js
@@ -0,0 +1,6 @@
+const arrayNum = [11,3,4,11,4,7,3];
+const setOfNum = [...new Set(arrayNum)];
+console.log(`Given array is: [${arrayNum}]`);
+console.log("----------------------After removing Duplicate elements------------------------");
+console.log(setOfNum);
+
